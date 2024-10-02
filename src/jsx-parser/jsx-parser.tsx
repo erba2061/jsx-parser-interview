1fd6b543648497446a5636e3a2a65618f1314642
Your Name <you@example.com>
2024-10-02 18:33:14 +0000

    Sandbox snapshot commit

--- src/jsx-parser/jsx-parser.tsx
+++ src/jsx-parser/jsx-parser.tsx
@@ -18,22 +18,82 @@ const tagReg = /<(\w+)(\/?)>/;
 
 const tagEndReg = /<\/(\w+)(?:\s+)?>/;
 
-type Token = {
-  t: "tag" | "tagEnd" | "text";
-};
+enum NodeType {
+  Tag,
+  TagEnd,
+  Text,
+}
 
-type Node = {
-  t: "component" | "text";
-};
+type TagNode = { node: NodeType.Tag; tag: string; closed: boolean };
+
+type TagEndNode = { node: NodeType.TagEnd; tag: string };
+
+type TextNode = { node: NodeType.Text; text: string };
 
 function newParser(value: string) {
   let pos = 0;
 
-  function peek() {}
+  function peek() {
+    return value[pos + 1];
+  }
 
-  function eat() {}
+  function peekSlice() {
+    return value.slice(pos);
+  }
 
-  function list() {}
+  function digest(part: string) {
+    if (value.slice(pos, part.length) !== part)
+      throw new Error(`digest issue ${part}`);
+    pos += part.length;
+  }
+
+  function skipWhitespace() {
+    let maybe = peek();
+    while (maybe === " " || maybe === "\n") {
+      digest(maybe);
+      maybe = peek();
+    }
+  }
+
+  function singularNode(): TagNode | TagEndNode | TextNode {
+    const nextCh = peek();
+
+    if (nextCh === '<') {
+      const match = peekSlice().match(tagReg);
+      if (match) {
+      digest(match[0]);
+      return { node: NodeType.Tag, tag: match[1], closed: !!match[2] };
+    }
+    }
+    if (node === NodeType.TagEnd) {
+      const match = value.slice(pos).match(tagEndReg);
+      if (!match) throw new Error(`expected tagEnd, got ${peek()}`);
+      digest(match[0]);
+      return { node: NodeType.TagEnd, tag: match[1] };
+    }
+    if (node === NodeType.Text) {
+      let char = peek();
+      let text = "";
+      while (!["<"].includes(char)) {
+        text = text + char;
+        digest(char);
+        char = peek();
+        if (char)
+      }
+    }
+  }
+
+  function itemNode() {}
+
+  function listNode() {
+    const store: ReactNode[] = [];
+    let node = itemNode();
+    while (node) {
+      store.push(node);
+      node = itemNode();
+    }
+    return store;
+  }
 
   function parse() {
     const list: ReactNode = [];
