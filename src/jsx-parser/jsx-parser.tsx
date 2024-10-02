5a370048f7ffd9d0781767dddf10eb4ce7c4db7e
Your Name <you@example.com>
2024-10-02 13:08:38 +0000

    xml parser

--- src/jsx-parser/jsx-parser.tsx
+++ src/jsx-parser/jsx-parser.tsx
@@ -1,4 +1,4 @@
-import { createElement } from "react";
+import { Fragment, createElement } from "react";
 import { AppShell, AppMenu, MenuItem, AppContent } from "./symbols";
 
 const symbolStore = {
@@ -14,7 +14,19 @@ interface JSXParserProps {
   value: string;
 }
 
+const children = (element) => {
+  const a = [];
+  for (const child of element.childNodes) {
+    if (child.nodeType === 1 && symbolStore[child.tagName])
+      a.push(createElement(symbolStore[child.tagName], {}, ...children(child)));
+    else a.push(child.textContent);
+  }
+  return a;
+};
+
 export const JSXParser = ({ value }: JSXParserProps) => {
-  // code here
-  return createElement("pre", {}, value);
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(value, "text/xml");
+
+  return createElement(Fragment, {}, ...children(xmlDoc));
 };
