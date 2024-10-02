940985b55c6eb28d2545a8dea9ab7379b4d16f88
Your Name <you@example.com>
2024-10-02 16:07:20 +0000

    Sandbox snapshot commit

--- src/jsx-parser/jsx-parser.tsx
+++ src/jsx-parser/jsx-parser.tsx
@@ -1,4 +1,4 @@
-import { createElement } from "react";
+import { ReactNode, createElement } from "react";
 import { AppShell, AppMenu, MenuItem, AppContent } from "./symbols";
 
 const symbolStore = {
@@ -14,7 +14,33 @@ interface JSXParserProps {
   value: string;
 }
 
+const tagReg = /<(\w+)(\/?)>/;
+
+const tagEndReg = /<\/(\w+)(?:\s+)?>/;
+
+type Token = {
+  t: "tag" | "tagEnd" | "text";
+};
+
+type Node = {
+  t: "component" | "text";
+};
+
+function newParser(value: string) {
+  let pos = 0;
+
+  function peek() {}
+
+  function eat() {}
+
+  function list() {}
+
+  function parse() {
+    const list: ReactNode = [];
+    return listNode();
+  }
+}
+
 export const JSXParser = ({ value }: JSXParserProps) => {
-  // code here
   return createElement("pre", {}, value);
 };
