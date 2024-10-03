d0ce8127c1ae7c709e87c0a600c9a3ac35b950e1
Your Name <you@example.com>
2024-10-03 11:57:33 +0000

    add lexer

--- src/jsx-parser/jsx-parser.tsx
+++ src/jsx-parser/jsx-parser.tsx
@@ -1,4 +1,4 @@
-import { Fragment, ReactNode, createElement } from "react";
+import { Fragment, ReactNode, createElement, useCallback } from "react";
 import { AppShell, AppMenu, MenuItem, AppContent } from "./symbols";
 
 const symbolStore = {
@@ -25,51 +25,18 @@ enum TokenType {
   EOF = "EOF",
 }
 
-interface Token {
-  token: TokenType;
-}
-
-interface TagToken extends Token {
-  token: TokenType.Tag;
-  tag: string;
-  closed: boolean;
-}
-
-interface TagEndToken extends Token {
-  token: TokenType.TagEnd;
-  tag: string;
-}
-
-interface TextToken extends Token {
-  token: TokenType.Text;
-  text: string;
-}
-
-interface EOFToken extends Token {
-  token: TokenType.EOF;
-}
-
-const tag = (tag: string, closed: boolean) => ({
-  token: TokenType.Tag,
-  tag,
-  closed,
-});
-const tagEnd = (tag: string) => ({ token: TokenType.TagEnd, tag });
-const text = (text: string) => ({ token: TokenType.Text, text });
-const EOF = () => ({ token: TokenType.EOF });
-
-function newParser(value: Token[]) {
-  let pos = 0;
+function newParser(lexer: Lexer) {
+  let currToken = lexer.getNextToken();
 
   function curr() {
-    return value[pos];
+    return currToken;
   }
 
   function eat(tokenType: TokenType) {
-    const token = value[pos];
+    const token = currToken;
     if (token.token !== tokenType)
       throw new Error(`expected token ${tokenType}, got ${token.token}`);
-    pos += 1;
+    currToken = lexer.getNextToken();
     return token;
   }
 
@@ -115,34 +82,118 @@ function newParser(value: Token[]) {
   return { parse };
 }
 
+const alphanumeric = /[0-9a-zA-Z]/;
+
 function newLexer(value: string) {
   let pos = 0;
-
-  function peek() {
-    return value[pos + 1];
-  }
 
   function curr() {
     return value[pos];
   }
 
-  function digest() {}
+  function skipWhitespace() {
+    while (curr() === " " || curr() === "\n") {
+      pos += 1;
+    }
+  }
 
-  function parse() {}
+  function getTag() {
+    if (curr() !== "<") throw new Error(`unexpected token ${curr()}`);
+    let closed = false;
+    let tag = "";
+    let resetPos = pos;
+    let end = false;
 
-  return { parse };
+    pos += 1;
+
+    if (curr() === "/") {
+      end = true;
+      pos += 1;
+    }
+
+    if (!alphanumeric.test(curr())) {
+      console.log("didnot pass stupid test");
+      pos = resetPos;
+      return;
+    }
+
+    let tagfilled = false;
+    while (curr() && curr() !== "/" && curr() !== ">") {
+      const peeked = curr();
+      console.log("peeked", peeked);
+      if (!tagfilled && alphanumeric.test(peeked)) {
+        tag = tag + peeked;
+        pos += 1;
+        continue;
+      }
+      tagfilled = true;
+      pos += 1;
+    }
+
+    if (!end && curr() === "/") {
+      closed = true;
+      pos += 1;
+    }
+
+    if (curr() !== ">") {
+      console.log("> issue", curr());
+      // throw new Error(`missing tag end, got ${curr()}`);
+      pos = resetPos;
+      return;
+    }
+
+    pos += 1;
+
+    return {
+      token: end ? TokenType.TagEnd : TokenType.Tag,
+      ...(!end && { closed }),
+      tag,
+    };
+  }
+
+  function getText() {
+    let text = curr();
+    pos += 1;
+    while (curr() && curr() !== "<") {
+      text = text + curr();
+      pos += 1;
+    }
+    return { token: TokenType.Text, text: text.trim() };
+  }
+
+  function getNextToken() {
+    if (pos > value.length - 1) {
+      return { token: TokenType.EOF };
+    }
+
+    const current = curr();
+
+    if (current === " " || current === "\n") {
+      skipWhitespace();
+      return getNextToken();
+    }
+
+    if (current === "<") {
+      const token = getTag();
+      if (token) return token;
+    }
+
+    return getText();
+  }
+
+  return { getNextToken };
 }
 
+type Lexer = ReturnType<typeof newLexer>;
+
 export const JSXParser = ({ value }: JSXParserProps) => {
-  const parser = newParser([
-    tag("AppContent", false),
-    tag("AppMenu", true),
-    text("Hello"),
-    tag("AppContent", false),
-    text("Hello"),
-    tagEnd("AppContent"),
-    tagEnd("AppContent"),
-    EOF(),
-  ]);
-  return createElement(Fragment, {}, ...parser.parse());
+  const parse = useCallback(() => {
+    try {
+      const parser = newParser(newLexer(value));
+      return parser.parse();
+    } catch (e) {
+      return [createElement("mark", {}, String(e))];
+    }
+  }, [value]);
+  return createElement(Fragment, {}, ...parse());
 };
