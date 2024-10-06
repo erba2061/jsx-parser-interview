94113b8f5d9d0c2b18fe30dfcd50906afd7bd1a9
Your Name <you@example.com>
2024-10-06 13:52:24 +0000

    Sandbox snapshot commit

--- src/jsx-parser/jsx-parser.tsx
+++ src/jsx-parser/jsx-parser.tsx
@@ -14,7 +14,88 @@ interface JSXParserProps {
   value: string;
 }
 
+type Token =
+  | { token: "tag"; single: boolean; end: boolean; name: string }
+  | { token: "text"; content: string };
+
+const newTextToken = (content: string) => ({ token: "text", content });
+const newTagToken = () => ({
+  token: "tag",
+  single: false,
+  end: false,
+  name: "",
+});
+
+let alphanumeric = /[a-zA-Z0-9]/;
+
+const newScope = () => {
+  return { scope, children: [], parent: null };
+};
+export const parseTokens = (value: string): string[] => {
+  const root = { scope: "global", data: [] };
+  const stack = [root];
+
+  let curr: any = { text: "" };
+
+  for (let pos = 0; pos < value.length; pos += 1) {
+    const char = value[pos];
+    const parent = stack.at(-1);
+    const tokens = parent.data;
+
+    if (char === "/") {
+      if (curr.tag) {
+        if (curr.text.length) {
+          curr.selfend = true;
+        } else {
+          curr.end = true;
+        }
+      } else {
+        curr.text = curr.text + char;
+      }
+      continue;
+    }
+
+    if (char === ">") {
+      if (curr.tag) {
+        if (curr.end) {
+          stack.pop();
+          curr = { text: "" };
+        } else if (curr.selfend) {
+          tokens.push(curr);
+          curr = { text: "" };
+        } else {
+          tokens.push(curr);
+          parent;
+          const scope = {
+            parent: tokens,
+            scope: curr.text.split(/\s/)[0],
+            data: [],
+          };
+          parent.children;
+          stack.push({});
+          curr = { text: "" };
+        }
+      }
+      continue;
+    }
+
+    if (char === "<") {
+      if (curr.text) tokens.push(curr);
+      curr = { text: "", tag: true };
+      continue;
+    }
+
+    if (char === " ") {
+      if (!curr.text) continue;
+    }
+
+    curr.text = curr.text + char;
+  }
+
+  return root;
+};
+
 export const JSXParser = ({ value }: JSXParserProps) => {
-  // code here
+  console.log(parseTokens(value));
   return createElement("pre", {}, value);
 };
